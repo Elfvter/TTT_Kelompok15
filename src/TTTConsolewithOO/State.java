ea008dffc795ac5e3f33d6bac07568d22c417088
wedha132 <gawedhaps@gmail.com>
2025-06-25 19:18:42 +0700

    commit5

--- src/TTTConsolewithOO/State.java
+++
@@ -1,8 +0,0 @@
-package TTTConsolewithOO;
-
-/**
- *  The enum State defines the various game states of the TTT game
- */
-public enum State {  // to save as "State.java"
-    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-}
